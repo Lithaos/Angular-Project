c4729311e8f60e437905d97b4ad6b373a0cd6f33
Lithaos <Wojto3@op.pl>
2019-02-12 16:57:46 +0100

    Lithaos

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,29 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+
+@Injectable()
 export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe("Nazwa","Cos tu jest napisane","https://tiny.pl/txzb1"),
-        new Recipe("Inna","Jest?","https://tiny.pl/txzb1")
+        new Recipe("Nazwa","Cos tu jest napisane","https://tiny.pl/txzb1",[
+            new Ingredient("Meat",1),
+            new Ingredient("French Fries", 20)
+        ]),
+        new Recipe("Inna","Jest?","https://tiny.pl/txzb1",[
+            new Ingredient("Buns",2),
+            new Ingredient("Meat",1)
+        ])
       ];
 
+      constructor(private slService: ShoppingListService){}
       getRecipes(){
           return this.recipes.slice();
+      }
+      
+      addIngredientsToShoppingList(ingredients:Ingredient[]){
+        this.slService.addIngredients(ingredients);
       }
 }
